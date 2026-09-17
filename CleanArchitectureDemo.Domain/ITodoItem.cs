0e37a6cf0b5c9e236cb45b7ddfe4e3539af2a3a1
A000917542 <56136004+A000917542@users.noreply.github.com>
2026-09-17 22:24:34 +0000

    initial clean architecture

---
+++ CleanArchitectureDemo.Domain/ITodoItem.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanArchitectureDemo.Domain;
+
+public interface ITodoItem
+{
+    public string Title { get; init; }
+    public bool IsCompleted { get; set; }
+}
---
+++ CleanArchitectureDemo.Domain/TodoItem.cs
@@ -0,0 +1,14 @@
+namespace CleanArchitectureDemo.Domain;
+
+public class TodoItem
+    : ITodoItem
+{
+    public TodoItem(string title)
+    {
+        this.Title = title;
+        this.IsCompleted = false;
+    }
+
+    public string Title { get; init; }
+    public bool IsCompleted { get; set; } = false;
+}
